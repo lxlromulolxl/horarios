3cdb42ed3e034d5630320eae814b9ae645152313
romulo.castro.oliveira@gmail.com <romulo.castro.oliveira@gmail.com>
2022-11-08 00:16:27 -0400

    deploy inicialr

---
+++ index.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+#criar 1 pagina do site
+
+# route ->
+
+
+
+
+# funcao
+@app.route("/")
+def hompage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def Contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return nome_usuario
+
+# colocar o site no ar
+
+if __name__ == "__main__":
+    app.run(debug=True)
